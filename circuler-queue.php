38f5a6140f8d2a9b2abde034ea1a4990f1dc1659
Rashed <rashedalamj@gmail.com>
2026-06-24 20:39:05 +0600

    Circuler Queue Implement and fix bug properly

--- circuler-queue.php
+++ circuler-queue.php
@@ -1,61 +1,83 @@
-<?php 
+<?php
 
-  //circuler_queue class
-  class Queue{
-      private $queue = [] ; //queue array
-      private $queue_size = 5 ; //maximum queue size
-      private $head = 0 ; //queue head pointer
-      private $tail = 0 ; //queue tail pointer
+class CirqulerQueue
+{
+    private array $queue = [];
+    private int $limit = 5;
+    private int $size = 0;
+    private int $head = 0;
+    private int $tail = 0;
 
-     //This function is enqueue new data in ($queue) array
-      function enqueue( int $data ){
-          if( ( ( $this->tail + 1 ) % ( $this->queue_size + 1 ) ) === $this->head ){
-             print("<br> Queue is full. <br>");
-             return ;
-          }
-          $this->queue[ $this->tail ] = $data ;
-          $this->tail = ( $this->tail + 1 ) % ( $this->queue_size + 1 ) ;
-      }
+    public function __construct(int $limit)
+    {
+        $this->limit = $limit;
+    }
 
-      //This function is dequeue from ($queue) array
-      function dequeue(){
-          if( $this->head === $this->tail ){
-            print("<br> Queue is empty. <br>");
-            return ;
-          }
-          $item = $this->queue[ $this->head ];
-          $this->head = ( $this->head + 1 ) % ( $this->queue_size + 1 ) ;
-          return $item ;
-      }
+    public function is_queue_full(): bool
+    {
+        if ($this->size === $this->limit) {
+            return true; // Queue is full
+        }
+        return false;
+    }
 
-      //This function is returned by current ($queue) array 
-      function get_current_queue(){
-          return $this->queue ;
-      }
+    public function is_queue_empty(): bool
+    {
+        if ($this->size === 0) {
+            return true; // Queue is empty
+        }
+        return false;
+    }
 
-  }
+    public function en_queue(int $data)
+    {
+        if (!$this->is_queue_full()) {
+            $this->queue[$this->tail] = $data;
+            $this->tail = ($this->tail + 1) % $this->limit;
+            $this->size = $this->size + 1;
+            return true; // Add new item in the queue
+        }
+        return false; // Queue is full;
+    }
 
- //queue object
-  $queue = new Queue();
+    public function peek()
+    {
+        return $this->queue[$this->head];
+    }
 
+    public function de_queue()
+    {
+        if (!$this->is_queue_empty()) {
+            $this->queue[$this->head] = null;
+            $this->head = ($this->head + 1) % $this->limit;
+            $this->size = $this->size - 1;
+            return true; // Queue item is remove
+        }
+        return false; // Queue is empty
+    }
+    public function print_queue()
+    {
+        print_r($this->queue);
+    }
+}
 
-  $queue->enqueue(1);
-  $queue->enqueue(2);
-  $queue->enqueue(3);
-  $queue->enqueue(4);
-  $queue->enqueue(5);
+$queue = new CirqulerQueue(limit: 5);
 
-  $queue->dequeue();
+$queue->en_queue(1);
+$queue->en_queue(2);
+$queue->en_queue(3);
+$queue->en_queue(4);
+$queue->en_queue(5);
 
-  $queue->enqueue(6);
+$queue->peek(); // 1
+$queue->de_queue();
 
-  $queue->dequeue();
+$queue->peek(); // 2
+$queue->de_queue();
 
-  $queue->enqueue(7);
+$queue->en_queue(6);
+$queue->en_queue(7);
 
-  echo "<pre>";
+$queue->en_queue(8); // false - Queue is full
 
-  print_r(
-      $queue->get_current_queue()
-   );
-?>
+$queue->print_queue();
